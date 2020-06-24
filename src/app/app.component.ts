5890548f5a6bff56259d1de5a9d378d2d62bf53b
unknown <yashkalavadiya1010@gmail.com>
2020-06-24 13:09:52 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {lorem} from 'faker'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Typing';
+  randomText = lorem.sentence();  
+  enteredText = '';
+
+  handleChange(val:string){    
+    this.enteredText = val;
+  }
+  compare(i:number){
+    if(this.enteredText[i] === this.randomText[i]){
+      return 'has-text-success'
+    }else if(i >= this.enteredText.length){
+      return 'has-text-grey'
+    }else{
+      return 'has-text-danger has-text-weight-bold'
+    }
+  }
 }
